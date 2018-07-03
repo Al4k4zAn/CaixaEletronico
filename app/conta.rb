448d82b6f637ac9915516b7959060abbf8d656d3
LuisChagas <rikeexx8@hotmail.com>
2018-07-03 16:33:15 -0300

    Ajuste no elsif

--- app/conta.rb
+++ app/conta.rb
@@ -9,8 +9,7 @@ class Conta
   def saca(valor)
     if valor > @saldo
       @mensagem_de_saida = 'Saldo insuficiente.'
-    elsif
-        valor > 700 # valor estipulado pela regra de negocio
+    elsif valor > 700 # valor estipulado pela regra de negocio
       @mensagem_de_saida = 'O valor máximo por transação é de 700 reais.'
     else
       @saldo -= valor # Reduz do saldo do cliente
